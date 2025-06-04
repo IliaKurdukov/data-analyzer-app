b419b6b47393c7f7e4c41abeb7e367fa929e739b
IliaKurdukov <79817899982@ya.ru>
2025-06-04 22:43:55 +0300

    Add files via upload

--- app_v2.py
+++ app_v2.py
@@ -62,33 +62,31 @@ if uploaded_file:
                 data = df[col]
               return(data)
 
-            def get_barplot(col):
-              data = process_multi_response_1(col)
-              plot_df=data.value_counts().div(n_resp/100)
-              plot_df = pd.DataFrame(plot_df)
-              plot_df.reset_index(names=['Ответ'], inplace=True)
-              plot_df['ans'] = plot_df['Ответ']
-              plot_df['ans'] = plot_df['ans'].apply(lambda x: 0 if x < 90 else x)
-              plot_df.sort_values(by=['ans', 'count'], ascending = [True, False], inplace=True)
-              plot_df['Ответ'] = plot_df['Ответ'].map(lambda x: meta.variable_value_labels[col][x])
-              plt.figure(figsize=(8, 4))
-              ax = sns.barplot(x=plot_df['count'], y=plot_df['Ответ'], color = '#E62083')
-              ax.spines['top'].set_visible(False)
-              ax.spines['right'].set_visible(False)
-              ax.spines['bottom'].set_visible(False)
-              ax.bar_label(ax.containers[0], label_type = 'edge', padding=5, fontsize=12, fmt = '%.1f%%')
-              plt.xticks([])
-              plt.yticks(fontsize=12)
-              question = meta.column_names_to_labels[col]
-              plt.title(f'{question}', fontsize=12, pad=10)
-              plt.xlabel('', fontsize=1)
-              plt.ylabel('')
+            #def get_barplot(col):
+            data = process_multi_response_1(col)
+            plot_df=data.value_counts().div(n_resp/100)
+            plot_df = pd.DataFrame(plot_df)
+            plot_df.reset_index(names=['Ответ'], inplace=True)
+            plot_df['ans'] = plot_df['Ответ']
+            plot_df['ans'] = plot_df['ans'].apply(lambda x: 0 if x < 90 else x)
+            plot_df.sort_values(by=['ans', 'count'], ascending = [True, False], inplace=True)
+            plot_df['Ответ'] = plot_df['Ответ'].map(lambda x: meta.variable_value_labels[col][x])
+            fig, ax = plt.subplots()
+            #plt.figure(figsize=(8, 4))
+            ax = sns.barplot(x=plot_df['count'], y=plot_df['Ответ'], color = '#E62083')
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
+            ax.spines['bottom'].set_visible(False)
+            ax.bar_label(ax.containers[0], label_type = 'edge', padding=5, fontsize=12, fmt = '%.1f%%')
+            plt.xticks([])
+            plt.yticks(fontsize=12)
+            question = meta.column_names_to_labels[col]
+            plt.title(f'{question}', fontsize=12, pad=10)
+            plt.xlabel('', fontsize=1)
+            plt.ylabel('')
 
             # Диаграмма
-            #fig, ax = plt.subplots()
-            #ax.hist(df[selected_col], bins=20)
-            #st.pyplot(fig)
-            get_barplot(col)
+            st.pyplot(fig)
 
     except Exception as e:
         st.error(f"Ошибка: {str(e)}")
