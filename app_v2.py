086d6e182f0d2c1237e26541193c018adfa3e131
IliaKurdukov <79817899982@ya.ru>
2025-06-06 01:02:00 +0300

    Add files via upload

--- app_v2.py
+++ app_v2.py
@@ -177,7 +177,7 @@ if uploaded_file:
               plot_df['Ответ'] = plot_df['Ответ'].map(lambda x: meta.variable_value_labels[col][x])
             
             fig, ax = plt.subplots()
-            #plt.figure(figsize=(5, 4))
+            plt.figure(figsize=(5, 4))
             ax = sns.barplot(x=plot_df['count'], y=plot_df['Ответ'].astype(str), color = '#E62083') #E62083 #12AFFF
             ax.spines['top'].set_visible(False)
             ax.spines['right'].set_visible(False)
@@ -195,6 +195,144 @@ if uploaded_file:
 
             # Диаграмма
             st.pyplot(fig)
+
+            def convert_to_intervals(col, df = df):
+                '''
+               Преобразовывает непрерывные значения в интервальные
+                '''
+                if col not in meta.variable_value_labels and df[col].nunique() > 5:
+                    if col == 'AGE':
+                        bins = [0, 18, 25, 35, 45, 55, 65, float('inf')]
+                        labels = ['до 18', '18 - 24', '25 - 34', '35 - 44', '45 - 54', '55 - 64', '65+']
+                        return pd.cut(df[col], bins = bins, labels=labels, right=False).astype('str')
+                    else:
+                        binned_col = pd.cut(df[col], bins = 5, right = False, duplicates='drop')
+                        return binned_col.apply(lambda x: f'{round(x.left)} - {round(x.right)}').astype('str')
+                return df[col]
+            
+            def process_multi_response(col1, col2):
+                '''
+                Объединяет столбцы в вопросах с множественным выбором ответов
+                '''
+                data = pd.DataFrame()
+                if is_multi_response(col1) and is_multi_response(col2):
+                    raise KeyError('Не поддерживается пересечение 2 вопросов с множественными ответами')
+                elif is_multi_response(col1) and not is_multi_response(col2) :
+                    for i in [key for key in meta.column_names_to_labels.keys() if key.__contains__(col1.split('_')[0])]:
+                         fict_df = df[[col2, i]]
+                         fict_df.columns = [col2, col1]
+                         data = pd.concat([data, fict_df], ignore_index=True)
+                elif not is_multi_response(col1) and is_multi_response(col2):
+                    for i in [key for key in meta.column_names_to_labels.keys() if key.__contains__(col2.split('_')[0])]:
+                        fict_df = df[[col1, i]]
+                        fict_df.columns = [col1, col2]
+                        data = pd.concat([data, fict_df], ignore_index=True)
+                else:
+                    data = df[[col1, col2]]
+                return data
+
+            def apply_value_labels(series, is_index=True):
+                '''
+                Применяет метки значений к столбцам таблицы, предварительно удалив из метаданных технические слова
+                '''
+                words_to_remove = ['(НЕ ЗАЧИТЫВАТЬ) ', '(ЗАЧИТАТЬ ПОСЛЕ ПАУЗЫ) ', '(ЗАЧИТАТЬ) ', '(ПОСЛЕ ПАУЗЫ) ']
+                pattern = "|".join(map(re.escape, words_to_remove))
+
+                for question, response in meta.variable_value_labels.items():
+                    for key, value in response.items():
+                        response[key] = re.sub(pattern, '', value)
+
+                if series.name in meta.variable_value_labels:
+                    return series.map(lambda x: meta.variable_value_labels[series.name].get(x, x))
+                return series
+
+            def create_crosstab(col1, col2, adjustment_type = 'holm', threshhold=0.05):
+
+                """
+                Создает таблицу сопряженности между двумя переменными с нормировкой по столбцам и вычисляет
+                статистические различия с поправкой на множественные сравнения.
+
+                Args:
+                    col1 (pd.Series): Первый столбец.
+                    col2 (pd.Series): Второй столбец.
+                    adjustment_type (str or None): Метод поправки на множественные сравнения ('holm', 'bonferroni', 'fdr_bh' и др.). Если None, поправка не применяется. По умолчанию 'holm'.
+                    threshhold (float): Порог уровня значимости для вывода результатов тестирования. По умолчанию 0.05.
+
+                Returns:
+                    dict: Словарь с двумя ключами:
+                        - 'table': стилизованная таблица сопряженности (pandas Styler object).
+                        - 'notes': текстовый вывод с результатами проверки статистических гипотез
+                                   и дополнительными комментариями.
+                """
+                data = process_multi_response(col1, col2)
+
+                transformed_col1 = convert_to_intervals(col1, data)
+                transformed_col2 = convert_to_intervals(col2, data)
+
+                # Для вывода таблицы
+                cross_df = pd.crosstab(transformed_col1,
+                                       transformed_col2,
+                                       margins=True,
+                                       margins_name='Всего',
+                                       normalize='columns')
+
+                cross_df.index = apply_value_labels(cross_df.index)
+                cross_df.columns = apply_value_labels(cross_df.columns)
+
+                table = cross_df.style.format(lambda x: round(x*100, 1))\
+                                      .background_gradient(cmap = 'Purples')\
+                                      .set_caption(f'Таблица сопряженности между {col1} и {col2}')
+
+                contingency_table = pd.crosstab(transformed_col1, transformed_col2)
+                p_values = []
+                comparisons = []
+
+                contingency_table.index = apply_value_labels(contingency_table.index)
+                contingency_table.columns = apply_value_labels(contingency_table.columns)
+
+                for i in range(contingency_table.shape[1]):
+                   for j in range(contingency_table.shape[0]):
+                        for k in range(j+1, contingency_table.shape[0]):
+                            success = [contingency_table.iloc[j, i], contingency_table.iloc[k, i]]
+                            nobs = [contingency_table.iloc[j, :].sum(axis=0), contingency_table.iloc[k, :].sum(axis=0)]
+                            if min(nobs) > 0:
+                                _, p_value = proportions_ztest(success, nobs)
+                                p_values.append(p_value)
+                                comparisons.append(f'{contingency_table.index[j]} vs {contingency_table.index[k]} в группе {contingency_table.columns[i]}')
+
+                if p_values:
+                    if adjustment_type is not None:
+                        reject, pvals_corrected, _, _ = multipletests(p_values, method=adjustment_type)
+                    else:
+                        pvals_corrected = p_values
+
+                    significance_comparisons = [
+                        f"{comp} (p = {p_value:.4f}{'*' if p_value < threshhold else ''})"
+                        for comp, p_value in zip(comparisons, pvals_corrected)
+                        if p_value < 0.1
+                    ]
+
+                    significance = "Значимые различия перечислены ниже \n" + "\n".join(significance_comparisons) if significance_comparisons else "Нет статистически значимых различий (p < 0.05)"
+                else:
+                    significance = "Не удалось вычислить z-тест (недостаточно данных)"
+
+                notes = f'''
+                Вывод о стат. значимости: \n {significance}
+                Примечание к таблице: 'Значения нормированы по столбцам и представлены в %.'
+                Расшифровка вопросов:
+                {col1} - {meta.column_names_to_labels[col1]}
+                {col2} - {meta.column_names_to_labels[col2]}
+                '''
+
+                return {'table': table,
+                        'notes': notes}
+
+            question2 = st.selectbox("Выберите уторой столбец для вывода таблицы сопряженности", list_of_questions)
+            col2 = meta_inside_out[question2]
+
+            result = create_crosstab(col, col2)
+            display(result['table'])
+            print(result['notes'])
 
     except Exception as e:
         st.error(f"Ошибка: {str(e)}")
